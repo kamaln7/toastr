4c3848c2f6b6049a50f31b098da949c39f376c43
Carlos Rodríguez <carloscsrm@gmail.com>
2016-03-19 19:25:42 +0100

    Update Toastr.php
    Remove htmlentites from the message to allow links in the notification's messages

--- src/Kamaln7/Toastr/Toastr.php
+++ src/Kamaln7/Toastr/Toastr.php
@@ -67,7 +67,7 @@ class Toastr {
             }
 
             // Toastr output
-            $output .= 'toastr.' . $notification['type'] . "('" . str_replace("'", "\\'", htmlentities($notification['message'])) . "'" . (isset($notification['title']) ? ", '" . str_replace("'", "\\'", htmlentities($notification['title'])) . "'" : null) . ');';
+            $output .= 'toastr.' . $notification['type'] . "('" .  $notification['message'] . "'" . (isset($notification['title']) ? ", '" . str_replace("'", "\\'", htmlentities($notification['title'])) . "'" : null) . ');';
         }
         $output .= '</script>';
 
